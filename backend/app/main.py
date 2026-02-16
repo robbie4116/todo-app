51e027ccda474b7fbeb6cc5cb456c2aacde97286
robbie4116 <juan_roberto_pineda@dlsu.edu.ph>
2026-02-16 14:20:23 +0800

    vercel domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Frontend URL
+    allow_origins=["http://localhost:5173", "https://todo-app-lgj6.onrender.com"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
