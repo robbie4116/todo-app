fe163e9e3d0d8727c48324c7253632da07045e50
robbie4116 <juan_roberto_pineda@dlsu.edu.ph>
2026-02-16 15:07:08 +0800

    cors config

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://todo-app-lgj6.onrender.com"],  # Frontend URL
+    allow_origins=["http://localhost:5173", "https://todo-app-lgj6.onrender.com", "https://todo-app-4116.vercel.app"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
